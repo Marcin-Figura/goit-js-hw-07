63cc33a4e1fcafe6a8f6b3604c885892e3daa78c
Marcin Figura <91680194+Marcin-Figura@users.noreply.github.com>
2022-02-28 10:05:28 +0100

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,37 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryBox = document.querySelector("div.gallery");
+
+console.log(galleryItems);
+
+for (const el of galleryItems) {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = el.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = el.preview;
+  galleryImage.dataset.source = el.original;
+  galleryImage.alt = el.description;
+
+  galleryBox.append(galleryItem);
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+};
+
+galleryBox.onclick = (event) => {
+event.preventDefault();
+
+  basicLightbox
+    .create(
+      `
+		<img width="1280" src = ${event.target.dataset.source}>
+	`
+    )
+    .show();
+};
